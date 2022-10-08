759f2b31fdfccc772743e2042757131ac5358817
mali4379 <68020493+mali4379@users.noreply.github.com>
2022-10-08 23:23:51 +0500

    Add files via upload

---
+++ Exer_1.js
@@ -0,0 +1,8 @@
+
+ /////////////////////////////Answer 1/////////////////////////////
+
+
+ document.write("Answer No 1");
+
+ let myname = "Muhammad Ali Roll Number PIAIC150191";
+ document.write("<br>", myname);
